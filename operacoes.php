34ced2624d23cbe2d9ba25901c5fca7dc8035791
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-05-13 17:25:06 -0300

    Introdução ao PHP

---
+++ operacoes.php
@@ -0,0 +1,14 @@
+<?php
+
+$idade = 33;
+$mais = 10;
+
+echo $idade ." anos";
+
+$soma = 2+2;
+$subtracao = 2-2;
+$multiplicacao = 2*2;
+$divisao = 2/2;
+
+echo "\nA soma é: ".$soma;
+echo "\nA subtracao é: ".$subtracao;
---
+++ tipos.php
@@ -0,0 +1,12 @@
+<?php
+//Tipos de dados primitivos
+$idade = 33;
+$salario = 1022.22;
+$divisao = 10/3;
+$texto = "Olá mundo!";
+$verdadeiro = true;
+$falso = false;
+
+//Exibir o tipo de dado de uma variavel:
+echo gettype($verdadeiro);
+
