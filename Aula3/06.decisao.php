cd53bb504c23ac5b5083f874fde8d5c7c40e1da5
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-05-14 11:03:25 -0300

    Aula4 - Condição (ELSE IF)

--- Aula3/06.decisao.php
+++ Aula3/06.decisao.php
@@ -1,14 +1,19 @@
 <?php
 
-$idade = 18;
-$nome = "Juliano";
+$idade = 17;
+$numeroDePessoas = 2;
+
 
 echo "Você só pode entrar se tiver mais do que 18 anos ou a partir de 16 anos acompanhado" .PHP_EOL;
-if ($idade == 18){//or = ||     and = &&
+if ($idade >= 18){//or = ||     and = &&
     echo "Você tem $idade anos." . PHP_EOL; 
     echo "Pode entrar";
+}
+else if($idade >= 16 && $numeroDePessoas > 1){
+    echo "Você tem $idade anos, está acompanhado, então pode entrar!";
 }
 else{
     echo "Não pode entrar!";
 }
-    
+
+
