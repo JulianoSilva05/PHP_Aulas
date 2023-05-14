e3c2484d40d7c7054b080669b0c7fd4ec0372475
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-05-13 23:07:51 -0300

    aula03

--- Aula3/06.decisao.php
+++ Aula3/06.decisao.php
@@ -1,12 +1,14 @@
 <?php
 
-$idade = 17;
+$idade = 18;
 $nome = "Juliano";
 
-echo "Você só pode entrar se tiver mais do que 18 anos e se seu nome for $nome!" .PHP_EOL;
-if ($idade == 18 && $nome == 'Juliano'){
+echo "Você só pode entrar se tiver mais do que 18 anos ou a partir de 16 anos acompanhado" .PHP_EOL;
+if ($idade == 18){//or = ||     and = &&
     echo "Você tem $idade anos." . PHP_EOL; 
     echo "Pode entrar";
 }
-else
+else{
     echo "Não pode entrar!";
+}
+    
