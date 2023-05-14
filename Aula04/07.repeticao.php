d6497210f5916098cc5122674c5f486ffbfc4de8
JulianoSilva05 <57194781+JulianoSilva05@users.noreply.github.com>
2023-05-14 11:52:03 -0300

    Aula4FOR

---
+++ Aula04/07.repeticao.php
@@ -0,0 +1,9 @@
+<?php
+$contador = 1;
+$fim = 15;
+
+while ($contador <= $fim){
+    echo $contador . PHP_EOL;
+    $contador = $contador + 2;
+}
+
--- Aula3/06.decisao.php
+++ Aula3/06.decisao.php
@@ -1,19 +1,30 @@
 <?php
 
-$idade = 19;
+$idade = 18;
 $numeroDePessoas = 2;
 
+echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+echo "a partir de 16 anos acompanhado" . PHP_EOL;
 
-echo "Você só pode entrar se tiver mais do que 18 anos ou a partir de 16 anos acompanhado" .PHP_EOL;
-if ($idade >= 18){//or = ||     and = &&
-    echo "Você tem $idade anos." . PHP_EOL; 
-    echo "Pode entrar";
-}
-elseif($idade >= 16 && $numeroDePessoas > 1){
-    echo "Você tem $idade anos, está acompanhado, então pode entrar!";
-}
-else{
-    echo "Não pode entrar!";
+if ($idade >= 18) {
+    echo "Você tem $idade anos. Pode entrar sozinho.";
+} elseif ($idade >= 16 && $numeroDePessoas > 1) {
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
+} else {
+    echo "Você só tem $idade anos. Você não pode entrar.";
 }
 
+echo PHP_EOL;
+echo "Adeus!";
 
+/*
+ssa aula, aprendemos sobre controle de fluxo (decisões) através do if:
+
+Podemos tomar uma decisão no código através do if, elseif e else
+if e elseif vão avaliar uma condição, se for verdadeira executam o bloco associado
+O if é o primeiro comando, elseif e else são opcionais
+O bloco elseif sempre precisa de um if antes
+O bloco else sempre precisa de um if ou elseif antes -O bloco else só executa se nenhuma das condições anteriores forem verdadeiras
+Através do && (AND lógico) e do || (OR lógico) podemos avaliar mais de uma condição
+
+*/
